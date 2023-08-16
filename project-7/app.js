5ab388a6b5a30b74229c8af845d45f20ae227eca
miyaadjoy <miyaadjoy177@gmail.com>
2023-08-16 23:18:48 +0600

    seventh project

--- project-7/app.js
+++ project-7/app.js
@@ -9,6 +9,132 @@
  * - user can also copy the rgb color code
  */
 
+const root = document.querySelector('#root');
+const changeColorButton = document.querySelector('#change-btn');
+const hexCoppyButton = document.querySelector('#copy-btn');
+const RGBCoppyButton = document.querySelector('#copy-btn2');
+const hexOutput = document.querySelector('#output');
+const RGBOutput = document.querySelector('#output2');
+const toastMessage = document.createElement('div');
+
+//Generating random numbers
+const createRandomNumbers = (min, max) => {
+  const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+  return randomNumber;
+};
+
+//creating random decimals for rgb colors
+const createColors = () => {
+  const red = createRandomNumbers(0, 255);
+  const green = createRandomNumbers(0, 255);
+  const blue = createRandomNumbers(0, 255);
+  return [red, green, blue];
+};
+
+//Generating hex colors
+const createHexColor = (r, g, b) => {
+  const red = r.toString(16).toUpperCase().padStart(2, 0);
+  const green = g.toString(16).toUpperCase().padStart(2, 0);
+  const blue = b.toString(16).toUpperCase().padStart(2, 0);
+
+  return `#${red}${green}${blue}`;
+};
+
+//Generating rgb colors
+const createRGBColor = (red, green, blue) => {
+  return `rgb(${red},${green},${blue})`;
+};
+//toast message appear animation
+const toastMessageAppear = () => {
+  toastMessage.classList.remove('slide-out');
+  toastMessage.classList.add('slide-in');
+};
+//toast message disappear animation
+const toastMessageDisappear = () => {
+  toastMessage.classList.remove('slide-in');
+  toastMessage.classList.add('slide-out');
+};
+//creating the toast message
+const createToastMessage = message => {
+  toastMessage.textContent = `${message} coppied!`;
+  toastMessage.classList.add('toast-message');
+  root.prepend(toastMessage);
+
+  toastMessageAppear();
+};
+
+//change background color
+const changeBackgroundColor = color => {
+  root.style.backgroundColor = color;
+  toastMessageDisappear();
+};
+
+//change hex output
+const changeHexOutput = color => {
+  hexOutput.value = color.slice(1);
+};
+//change rgb output
+const changeRGBOutput = color => {
+  RGBOutput.value = color;
+};
+
+//hex code validation
+/**
+ *
+ * @param {string} color
+ * @returns
+ */
+const isValidHex = color => {
+  if (color.length !== 6) return false;
+  if (color[0] === '#') return false;
+
+  return /^[0-9A-Fa-f]{6}$/i.test(color); //RegExp(Regular expression);
+};
+
+//click event on change color button
+changeColorButton.addEventListener('click', () => {
+  const [r, g, b] = createColors();
+
+  changeHexOutput(createHexColor(r, g, b));
+  changeRGBOutput(createRGBColor(r, g, b));
+  //changing background color
+  changeBackgroundColor(createHexColor(r, g, b));
+});
+
+/*
+const html = `<button class="toast-message hide"> ${hexOutput.value} coppied!</button>`;
+root.insertAdjacentHTML('afterBegin', html);
+*/
+
+//click event on hex color copy button
+hexCoppyButton.addEventListener('click', () => {
+  if (isValidHex(hexOutput.value)) {
+    window.navigator.clipboard.writeText(hexOutput.value);
+    //toast message
+    createToastMessage(`#${hexOutput.value}`);
+  } else alert('Invalid hex code');
+});
+
+//click event on rgb color copy button
+RGBCoppyButton.addEventListener('click', () => {
+  window.navigator.clipboard.writeText(RGBOutput.value);
+  //toast message
+  createToastMessage(RGBOutput.value);
+});
+
+//click event on toast message element
+toastMessage.addEventListener('click', toastMessageDisappear);
+
+//keyup event on color input
+hexOutput.addEventListener('keyup', e => {
+  const color = e.target.value;
+  if (color) {
+    if (isValidHex(color)) changeBackgroundColor('#' + color);
+    hexOutput.value = color.toUpperCase();
+  }
+});
+
+/*
 let div = null;
 
 window.onload = () => {
@@ -103,7 +229,10 @@ function generateToastMessage(msg) {
 /**
  * @param {string} color : ;
  */
+/*
 function isValidHex(color) {
   if (color.length !== 6) return false;
   return /^[0-9A-Fa-f]{6}$/i.test(color);
 }
+
+*/
